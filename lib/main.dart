aeb9243d05f1db221f634de4632199f933920e0c
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-15 10:49:49 +0800

    Add basic layouting for 'Products' grid and item widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:fp_shop_app/screens/products_overview_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,24 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Shop'),
-      ),
-      body: const Center(
-        child: Text(
-          // ignore: avoid_escaping_inner_quotes
-          'Let\'s build a shop!',
-        ),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
