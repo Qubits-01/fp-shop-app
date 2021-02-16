f0e573fd01096153ab083bf57e0dcbb8efb0c198
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-16 10:00:05 +0800

    Add shopping cart data model

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:fp_shop_app/models/cart_item.dart';
+
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      // change quantity...
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
