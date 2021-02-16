d8340eb63c536fde8b624103f21fd2b3dec7d3dd
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-16 10:20:27 +0800

    Implement multiple provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'package:fp_shop_app/providers/cart_provider.dart';
+import 'package:fp_shop_app/providers/products_provider.dart';
 import 'package:fp_shop_app/screens/products_overview_screen.dart';
 import 'package:fp_shop_app/screens/product_detail_screen.dart';
-import 'package:fp_shop_app/providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => CartProvider(),
+        ),
+      ],
       child: MaterialApp(
         title: 'My Shop',
         theme: ThemeData(
