5d43c0e5c80269946f7168469c2376a1cd805968
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-17 08:40:01 +0800

    Add snackbar on add to cart and an UNDO action in it

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -60,6 +60,24 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to cart!',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: const Duration(
+                    seconds: 2,
+                  ),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
           ),
         ),
