bb462f4c66e78e350a8220643dc216a7d258f044
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-19 18:41:42 +0800

    Update product data via PATCH request on the server

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -137,10 +137,23 @@ class ProductsProvider with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, ProductProvider newProduct) {
+  Future<void> updateProduct(String id, ProductProvider newProduct) async {
     final int prodIndex = _items.indexWhere((prod) => prod.id == id);
 
     if (prodIndex >= 0) {
+      final url =
+          'https://p3-inc-default-rtdb.firebaseio.com/products/$id.json';
+
+      await http.patch(
+        url,
+        body: json.encode({
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'imageUrl': newProduct.imageUrl,
+          'price': newProduct.price,
+        }),
+      );
+
       _items[prodIndex] = newProduct;
 
       notifyListeners();
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -113,14 +113,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
 
     if (_editedProduct.id != null) {
-      Provider.of<ProductsProvider>(context, listen: false)
+      await Provider.of<ProductsProvider>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-
-      setState(() {
-        _isLoading = false;
-      });
-
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<ProductsProvider>(context, listen: false)
@@ -141,14 +135,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-
-        Navigator.of(context).pop();
+        // } finally {
+        //   setState(() {
+        //     _isLoading = false;
+        //   });
+        //
+        // Navigator.of(context).pop();
       }
     }
+    setState(() {
+      _isLoading = false;
+    });
+
+    Navigator.of(context).pop();
   }
 
   @override
